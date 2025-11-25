fcc5644a76214532220f362c8d8a91b6a3dcaa74
arshad-dev57 <qureshiarshad947@gmail.com>
2025-11-24 22:43:20 -0800

    worked on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,12 @@ import 'package:lyno_cms/screens/dashboard_screen.dart';
 import 'package:lyno_cms/screens/login_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // final prefs = await SharedPreferences.getInstance();
 
-  // Get the token from SharedPreferences
-  String? token = prefs.getString('token');
-  print(token);
-  runApp(MyApp(token: token));
+  // String? token = prefs.getString('token');
+  // print(token);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,8 +29,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFF6F6F7),
         textTheme: GoogleFonts.interTextTheme(),
       ),
-      // Navigate to Dashboard if token exists, otherwise show Login Screen
-      home: token != null ? DashboardScreen() : LoginPage(),
+      home: LoginPage(),
     );
   }
 }
